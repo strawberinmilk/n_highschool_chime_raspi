82fbf2fc5e7b674a8e9328c3765ae9cfa6dbbe60
rinkeisgithub <rinkei.krt6006@gmail.com>
2018-02-16 15:11:05 +0900

    refacta

--- index.js
+++ index.js
@@ -10,70 +10,70 @@ const cron = require("cron").CronJob
 let status = 0;
 let pin = 4;
 //すでにセットアップデータがあると死ぬのでtry-catch必須
-try{
+try {
   fs.writeFileSync('/sys/class/gpio/export', pin);
-  fs.writeFileSync('/sys/class/gpio/gpio' + pin + '/direction', 'out');  
-}catch(e){
+  fs.writeFileSync('/sys/class/gpio/gpio' + pin + '/direction', 'out');
+} catch (e) {
 }
 
 //チャイム鳴らすタイミングで実行する関数
-function chime (){
-  player.play("./chime.mp3",err =>{
-    if (err)throw err;
+function chime() {
+  player.play("./chime.mp3", err => {
+    if (err) throw err;
   });
   fs.writeFileSync('/sys/class/gpio/gpio' + pin + '/value', status);
 }
 
-  new cron('0 30 9 * * *', function () {
-    status = 0;
-    chime();
-  }).start();
-  new cron('0 30 10 * * *', function () {
-    status = 1;
-    chime();
-  }).start();
-  new cron('0 40 10 * * *', function () {
-    status = 0;
-    chime();
-  }).start();
-  new cron('0 30 11 * * *', function () {
-    status = 1;
-    chime();
-  }).start();
-  new cron('0 40 11 * * *', function () {
-    status = 0;
-    chime();
-  }).start();
-  new cron('0 30 12 * * *', function () {
-    status = 1;
-    chime();
-  }).start();
-  new cron('0 10 13 * * *', function () {
-    status = 0;
-    chime();
-  }).start();
-  new cron('0 40 13 * * *', function () {
-    status = 1;
-    chime();
-  }).start();
-  new cron('0 50 13 * * *', function () {
-    status = 0;
-    chime();
-  }).start();
-  new cron('0 40 14 * * *', function () {
-    status = 1;
-    chime();
-  }).start();
-  new cron('0 50 14 * * *', function () {
-    status = 0;
-    chime();
-  }).start();
-  new cron('0 40 15 * * *', function () {
-    status = 1;
-    chime();
-  }).start();
-  new cron('0 0 18 * * *', function () {
-    status = 0;
-    chime();
-  }).start();
+new cron('0 30 9 * * *', function () {
+  status = 0;
+  chime();
+}).start();
+new cron('0 30 10 * * *', function () {
+  status = 1;
+  chime();
+}).start();
+new cron('0 40 10 * * *', function () {
+  status = 0;
+  chime();
+}).start();
+new cron('0 30 11 * * *', function () {
+  status = 1;
+  chime();
+}).start();
+new cron('0 40 11 * * *', function () {
+  status = 0;
+  chime();
+}).start();
+new cron('0 30 12 * * *', function () {
+  status = 1;
+  chime();
+}).start();
+new cron('0 10 13 * * *', function () {
+  status = 0;
+  chime();
+}).start();
+new cron('0 40 13 * * *', function () {
+  status = 1;
+  chime();
+}).start();
+new cron('0 50 13 * * *', function () {
+  status = 0;
+  chime();
+}).start();
+new cron('0 40 14 * * *', function () {
+  status = 1;
+  chime();
+}).start();
+new cron('0 50 14 * * *', function () {
+  status = 0;
+  chime();
+}).start();
+new cron('0 40 15 * * *', function () {
+  status = 1;
+  chime();
+}).start();
+new cron('0 0 18 * * *', function () {
+  status = 0;
+  chime();
+}).start();
 
